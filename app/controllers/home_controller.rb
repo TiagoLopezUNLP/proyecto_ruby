20311aef4e105c0d358ef576ddb39be0fe43af64
ivanskrt <ivanskrt1@gmail.com>
2025-12-04 01:59:14 +0000

    Mejorar diseño de tienda con filtros y vista de detalle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
   
-def index
+  def index
+    @q = Producto.activos.ransack(params[:q])
+    @productos = @q.result(distinct: true)
+                   .includes(:autor, :categoria, imagenes_attachments: :blob)
+                   .order(created_at: :desc)
+                   .paginate(page: params[:page], per_page: 12)
   end
 end
